4756d2f976ac338f2fb33e59a2f1b165f323a6ae
Timofey Tiunov <tiunov@mindbox.cloud>
2025-05-15 15:56:35 +0400

    form and rendering together

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,21 @@
 import { createGame } from "./game.ts";
+import renderParamsForm from "./renderParamsForm.ts";
 import renderGameField from "./renderGameField.ts";
 
 const root = document.getElementById("app")!;
 
-// let intervalId: number | null = null;
+let intervalId: number | undefined;
 
-// const form = renderForm(({ fieldSize, intervalMs }) => {
-//   if (intervalId !== null) {
-//     clearInterval(intervalId);
-//   }
-//   updateField(fieldSize, root, fieldElementId);
+const form = renderParamsForm((gameSettings) => {
+  clearInterval(intervalId);
 
-//   intervalId = setInterval(() => {
-//     updateField(fieldSize, root, fieldElementId);
-//   }, intervalMs);
-// });
+  const game = createGame(gameSettings);
+  renderGameField(game.state, root);
 
-// root.append(form);
-
-const game = createGame({
-  fieldSize: 10,
-  intervalMs: 500,
-  armies: [
-    {
-      size: 4,
-      hp: 50,
-      strength: 1,
-      visionLength: 3,
-      criticalPercent: 10,
-      color: 'blue',
-    },
-    {
-      size: 7,
-      hp: 50,
-      strength: 1,
-      visionLength: 3,
-      criticalPercent: 10,
-      color: 'red',
-    }
-  ],
+  intervalId = setInterval(() => {
+    game.makeTurn();
+    renderGameField(game.state, root);
+  }, gameSettings.intervalMs);
 });
 
-renderGameField(game.state, root);
-
-setInterval(() => {
-  game.makeTurn();
-  renderGameField(game.state, root);
-}, 500);
+root.append(form);
