5787ecabf32b10172d1afcaa732484ff58d49bc0
Timofey Tiunov <tiunov@mindbox.cloud>
2025-05-15 15:47:18 +0400

    field rendering added

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,49 @@
-import renderForm from "./renderParamsForm.ts";
-import updateField from "./updateField.ts";
+import { createGame } from "./game.ts";
+import renderGameField from "./renderGameField.ts";
 
 const root = document.getElementById("app")!;
-const fieldElementId = "field";
 
-let intervalId: number | null = null;
+// let intervalId: number | null = null;
 
-const form = renderForm(({ fieldSize, intervalMs }) => {
-  if (intervalId !== null) {
-    clearInterval(intervalId);
-  }
-  updateField(fieldSize, root, fieldElementId);
+// const form = renderForm(({ fieldSize, intervalMs }) => {
+//   if (intervalId !== null) {
+//     clearInterval(intervalId);
+//   }
+//   updateField(fieldSize, root, fieldElementId);
 
-  intervalId = setInterval(() => {
-    updateField(fieldSize, root, fieldElementId);
-  }, intervalMs);
+//   intervalId = setInterval(() => {
+//     updateField(fieldSize, root, fieldElementId);
+//   }, intervalMs);
+// });
+
+// root.append(form);
+
+const game = createGame({
+  fieldSize: 10,
+  intervalMs: 500,
+  armies: [
+    {
+      size: 4,
+      hp: 50,
+      strength: 1,
+      visionLength: 3,
+      criticalPercent: 10,
+      color: 'blue',
+    },
+    {
+      size: 7,
+      hp: 50,
+      strength: 1,
+      visionLength: 3,
+      criticalPercent: 10,
+      color: 'red',
+    }
+  ],
 });
 
-root.append(form);
+renderGameField(game.state, root);
+
+setInterval(() => {
+  game.makeTurn();
+  renderGameField(game.state, root);
+}, 500);
---
+++ src/renderGameField.ts
@@ -0,0 +1,45 @@
+import { IGame } from './game';
+
+const FIELD_ELEMENT_ID = 'game-field';
+
+function createNewField(size: number) {
+  const newField = document.createElement('div');
+  newField.classList.add('field-root');
+  newField.style.setProperty('--n', size.toString());
+  newField.id = FIELD_ELEMENT_ID;
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('field-cell');
+      newField.append(cell);
+    }
+  }
+
+  return newField;
+}
+
+function getOrCreateFieldElement(size: number, root: HTMLElement) {
+  const existingField = document.getElementById(FIELD_ELEMENT_ID);
+
+  if (existingField && existingField.style.getPropertyValue('--n') === size.toString()) {
+    return existingField;
+  }
+
+  existingField?.remove();
+  const newField = createNewField(size);
+  root.append(newField);
+  return newField;
+}
+
+export default function (state: IGame['state'], root: HTMLElement) {
+  const field = getOrCreateFieldElement(state.length, root);
+
+  for (let i = 0; i < state.length; i++) {
+    for (let j = 0; j < state[i].length; j++) {
+      const cell = field.children[i * state.length + j] as HTMLDivElement;
+      const cellSoldier = state[i][j];
+      cell.style.backgroundColor = cellSoldier ? cellSoldier.army.color : 'white';
+    }
+  }
+}
